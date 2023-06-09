147a9317234ba97af14a2462686d0a4d00fe988f
Gustavo Sousa <gustavohl98@gmail.com>
2023-06-09 10:55:12 -0300

    feat: finish input form

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import MacrosCalcForm from './MacrosCalcForm';
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-between p-12">
+    <div className="flex min-h-screen flex-col items-center justify-start p-12">
       <header className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Sua dieta ideal na ponta do&nbsp;
